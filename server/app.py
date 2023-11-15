47d014fbd8fcc9d52ef6e9fd025697eeac92d3b1
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-15 16:05:17 +0300

    First Flask App

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Welcome to the Home Page!</h1>'
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1> This is {username}\'s profile. </h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
